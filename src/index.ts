02ff0c62aaaaa5bdcb4a5ba02da97bca60cacaf7
chenshuai2144 <qixian.cs@outlook.com>
2020-02-23 11:57:44 +0800

    feat: migrate to umi 3

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import fs from 'fs';
 import path from 'path';
-import { IApi } from 'umi-types';
+import { IApi } from 'umi';
 
 export default function(api: IApi, options) {
-  api.onBuildSuccess(() => {
+  api.onBuildComplete(err => {
+    if (err) {
+      return;
+    }
     // 创建一个 404 文件，因为 github pages 不支持单应用模式
     const { absOutputPath } = api.paths;
     fs.copyFileSync(path.join(absOutputPath, 'index.html'), path.join(absOutputPath, '404.html'));
-    api.log.success('create 404.html');
+    api.logger.info('create 404.html');
   });
 }
